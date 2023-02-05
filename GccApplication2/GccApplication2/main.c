f0f8bbf244efd2d5be7caff9d799d0d8816c89c5
José Carlos Tejeda Guzmán <64748878+JoseTejedaGuzman@users.noreply.github.com>
2023-02-05 23:31:05 +0100

    Add files via upload

---
+++ GccApplication2/GccApplication2/main.c
@@ -0,0 +1,35 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 9/20/2022 3:15:26 PM
+ * Author : jose_
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 16000000UL    //Define CPU clock as 16Mhz
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+
+int main(void){
+	
+	
+	DDRD = 0xFF;
+	PORTD = 0xFF;
+	TCCR1B = (1 << CS02) | (0 << CS01) | (0 << CS00);
+	TIMSK1 = 0b00000001;
+	
+	sei();
+	while (1){};
+
+	
+
+}
+
+ISR(TIMER1_OVF_vect)
+{
+	PORTD ^= 0xFF;
+
+}
+
+
