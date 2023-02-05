bff76e71d85e1bb6b584641c9e5b000a962a8e07
José Carlos Tejeda Guzmán <64748878+JoseTejedaGuzman@users.noreply.github.com>
2023-02-05 22:31:37 +0100

    Add files via upload

---
+++ AssemblerApplication1/AssemblerApplication1/AssemblerApplication1.asm
@@ -0,0 +1,61 @@
+.include "m328def.inc" 
+.org 0x0000
+           RJMP begin; jump to begin 
+.ORG 0X0002 
+			RJMP Interrupt;
+.org 0x0034 
+ begin:    CLI
+           LDI    R16,low(RAMEND)
+		   OUT    SPL,R16
+           LDI    R16,high(RAMEND)
+		   OUT    SPH, R16   
+		                
+		   LDI R16, (0<<ISC01)|(1<<ISC00)
+		   STS EICRA, R16
+
+		   LDI R16, (1<<INT0)
+		   OUT EIMSK, R16
+
+		   LDI    R16,0xFF 
+           OUT    DDRB, R16 
+
+		   sei
+		     
+main:
+           LDI    R16,0xFF 
+           OUT    PORTB, R16
+
+           RCALL  Delay
+
+		   LDI	  R16, 0xFF
+           OUT    PORTB, R16
+
+           RCALL  Delay
+		   RJMP   main
+
+Interrupt: 
+
+		   LDI    R20, 0x0000
+		   OUT    PORTB, R20
+
+		   RCALL Delay
+
+		   RETI
+
+
+Delay:     LDI    R17, 0x9E
+		   LDI    R18, 0x9E
+		   LDI    R19, 0x9E
+
+loop:      DEC    R17 
+           BRNE   loop 
+		   
+loop2:	   LDI R17, 0x9E
+		   DEC R18
+		   BRNE loop
+
+loop3:		  LDI R18, 0x9E
+		  DEC R19
+		  BRNE   loop
+		               
+          RET  
